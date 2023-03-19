f1f9c7a4cd09c572d8d2b6f98ae02720190c15db
Adam Todd <toddadam13@gmail.com>
2023-03-19 17:29:36 -0400

    v0.1
    Can generate questions on first page

--- resources/js/2023.js
+++ resources/js/2023.js
@@ -4,30 +4,69 @@ const form = {
     title: Title given to doc
     prematch, auto, tele, endgame: pages, add as many as you want
     {
-      "q": question to ask
-      "id": id given, must be unique
-      "type": type of question; mcq, txt, num
+      q: question to ask
+      id: id given, must be unique
+      type: type of question; mcq, txt, num
         mcq: multiple choice, requires "options"
-          "options": answers for mcq, formatted: {"return value":"public name"}
+          choices: answers for mcq, formatted: {"return value":"public name"}
         txt: any text, used as string
         int: any integer
-          "min": minimum valid input
-          
-          "max": maximum valid input"
+          min: minimum valid input 
+          max: maximum valid input
+      required: true or false
     }
   */
 
   title: "Charged Up 263 Scouting",
-  prematch: [
-    { 
-      q: "Your name:",
-      id: "name",
-      type: "txt"
-    }, 
+  pages: [
     {
-      q: "Match Number:",
-      id: "match",
-      type: "txt"
-    }
+      subtitle: "Prematch",
+      questions: [
+        { 
+          q: "txt:",
+          id: "name",
+          type: "txt",
+          required: "true"
+        }, 
+        {
+          q: "mcq:",
+          id: "match",
+          type: "mcq",
+          choices: [
+            {
+              choice: "sussy"
+            },
+            {
+              choice: "balls"
+            }
+          ],
+          required: "true"
+        },
+        {
+          q: "int:",
+          id: "match",
+          type: "int",
+          min: "10",
+          max: "100",
+          required: "true"
+        }
+      ]
+    },
+    {
+      subtitle: "Autonomous",
+      questions: [
+        { 
+          q: "Your name:",
+          id: "name",
+          type: "txt",
+        }, 
+        {
+          q: "Match Number:",
+          id: "match",
+          type: "txt"
+        }
+      ]
+    },
   ]
 }
+
--- resources/js/mysite.js
+++ resources/js/mysite.js
@@ -1,23 +1,80 @@
-function next(output) {
+function generateVisiblePage() {
 
-}
+};
 
-function prev(output) {
+function generateInvisiblePage() {
 
-}
+};
 
-function scanReset(output) {
+function generatePages() {
 
-}
+  let formItem = document.getElementById('main')
+  let page = '';
 
-function saveReset(output) {
+  //Creates the first page and displays normally
+  page += '<ol class="page" class="visible">'
+  page += '<h2>' + form.pages[0].subtitle + '</h2>';
 
-}
+  let q; let id; let type; let min; let max; let choices = [1,2,3];
 
-function start() {
+  for (var i = 0; i < form.pages[0].questions.length; i++) {
+    q = form.pages[0].questions[i].q;
+    id = form.pages[0].questions[i].id;
+    type = form.pages[0].questions[i].type;
+    req = form.pages[0].questions[i].required;
 
-}
+    page += '<li>';
+    page += ('<label for="' + id + '">' + q + '</label>');
 
-windows.onload = function () {
+    if (type == "txt") {
+      if (req == "true") {page += ('<input name="' + id + '" required type="text"></input');}
+      else  {page += ('<input name="' + id + '" type="text"></input');}
 
-}
+    } else if (type == "mcq") {
+      choices = form.pages[0].questions[i].choices;
+
+      for (var j = 0; j < choices.length; j++) {
+        if (req == "true") {
+          page += ('<label for="' + choices[j].choice + '">' + choices[j].choice + '</label>')
+          page += ('<input name="' + id + '" required type="radio" value="' + choices[j].choice + '"></input');
+          
+        }
+        else  {
+          page += ('<input name="' + id + '" type="radio"></input');
+        }
+
+      }
+    } else if (type == "int") {
+      min = form.pages[0].questions[i].min
+      max = form.pages[0].questions[i].max
+
+      if (req == "true") {page += ('<input name="' + id + '" required type="number" min="' + min + '" max="' + max + '"></input');}
+      else  {page += ('<input name="' + id + '" type="number" min="' + min + '" max="' + max + '"></input');}
+
+    } else if (type == "special") {
+      //ADAM MANUALLY IMPLEMENT THIS FOR THE SCORE TRACKER, MAKE SOME AUTO THING LATER
+      
+    } else {
+      alert("Error! Invalid question type! Fix yer json!!")
+    }
+    page += '</li>'
+  };
+
+  page += '</ol>'
+  formItem.innerHTML += page;
+};
+
+function initialize() {
+
+  //Sets title to <header> title and text title
+  document.title=form.title;
+  document.getElementsByTagName('h1')[0].innerHTML=form.title;
+
+  //Generates pages
+  generatePages();
+};
+
+window.onload = function() {
+  var output;
+  initialize();
+};
